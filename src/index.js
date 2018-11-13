66ac52381d9b42f9ebf3006c91f6cbab48e31ea8
Ash Laidlaw <ash.laidlaw@vacasa.com>
2018-11-13 15:23:34 -0800

    Add simple clicker to make sure app works

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      clicks: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({ clicks: prevState.clicks + 1 }));
   }
 
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Button addOne={this.handleClick} />
+            <p>Number of clicks: {this.state.clicks}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App name='Ash' />, document.getElementById('app'));
+const Button = props => <button className="btn btn-sm btn-primary" onClick={props.addOne}>Click Me!</button>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
